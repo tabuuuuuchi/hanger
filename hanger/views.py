c46c9d745327ae721d96eb2f0b0a1849eec3e179
youta tabuchi <tabuchiyouta0717@gmail.com>
2026-02-04 04:11:04 +0900

    アイテム編集機能実装

--- hanger/views.py
+++ hanger/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, DeleteView
+from django.urls import reverse ,reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from .models import Item, Season, Outfit
 
 
@@ -31,6 +31,19 @@ class DeleteItemView(DeleteView):       #アイテム詳細
     model = Item
     success_url = reverse_lazy('list-item')
 
+
+class UpdateItemView(UpdateView):
+    template_name = 'hanger/item_update.html'
+    model = Item
+    fields = ('name', 'brand', 'category', 'thumbnail')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('detail-item', kwargs={'pk': self.object.id})
+    
 
 class ListOutfitView(ListView):     #コーデ一覧
     template_name = 'hanger/outfit_list.html'
